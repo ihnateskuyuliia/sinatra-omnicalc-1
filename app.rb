e2af25e455562de6eeb3c506a61b76b8e6e3c702
ihnateskuyuliia <yuliiaihnatesku@uchicago.edu>
2024-10-20 21:19:50 +0000

    1

--- app.rb
+++ app.rb
@@ -13,6 +13,12 @@ get("/square/new") do
   erb(:new_square_calc)
 end
 
+get '/square/results' do
+  @the_num = params.fetch("users_number").to_f
+  @the_result = @the_num ** 2
+  erb(:square_results)
+end
+
 get("/") do
   "
   <h1>Welcome to your Sinatra App!</h1>

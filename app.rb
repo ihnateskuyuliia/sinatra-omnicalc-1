5753f83fb852d9a668cb432ecfd6e2c1f09eb52a
ihnateskuyuliia <yuliiaihnatesku@uchicago.edu>
2024-10-21 00:27:27 +0000

    DONE!

--- app.rb
+++ app.rb
@@ -21,7 +21,7 @@ get '/square/results' do
 end
 #TEMPLATE
 
-#
+#ROOT
 get("/square_root/new") do
   erb(:new_root_calc)
 end
@@ -31,7 +31,7 @@ get '/square_root/results' do
   @the_result = Math.sqrt(@the_num)
   erb(:root_results)
 end
-#
+#ROOT
 
 #RANDOM
 get("/random/new") do
@@ -45,7 +45,41 @@ get '/random/results' do
   @result = rand(@minimum..@maximum)
   erb(:random_results)
 end
+#RANDOM
 
+
+#PAYMENT
+get("/payment/new") do
+  erb(:payment_calc)
+end
+
+get '/payment/results' do
+  @apr = params.fetch("user_apr").to_f
+  @r = @apr / 100 / 12
+  @years = params.fetch("user_years").to_i
+  @value = params.fetch("user_pv").to_f
+  
+  # Calculate the number of monthly periods
+  @n = @years * 12
+
+  # Organize the numerator and denominator according to the formula
+  @numerator = @r * @value
+  @denominator = 1 - (1 + @r) ** -@n
+
+  # Calculate the monthly payment (P)
+  @monthly_payment = @numerator / @denominator
+
+  #Format the Principal Value
+  @value_formatted = @value.to_fs(:currency)
+  
+  # Format the result as currency
+  @formatted_payment = @monthly_payment.to_fs(:currency)
+
+  #Format the APR
+  @apr_formatted = @apr.to_fs(:percentage, {:precision => 4})
+  erb(:payment_results)
+end
+#PAYMENT
 
 get("/") do
   "
